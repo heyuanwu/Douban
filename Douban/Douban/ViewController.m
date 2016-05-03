b7d82954bac7077c45ebf5d2654ca7cde5f8bbca
lkjy <lkjy>
2016-05-03 18:29:48 +0800

    我编写了一个输出

---
+++ Douban/Douban/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Douban
+//
+//  Created by lkjy on 16/5/3.
+//  Copyright © 2016年 heyuanwu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"dfsdfsdfdsfdsf");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
