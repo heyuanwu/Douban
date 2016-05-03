7b3fcba95db0c5eb7f447a6062e25edf5b24b9f5
lkjy <lkjy>
2016-05-03 19:43:31 +0800

    非官方大哥

--- Douban/Douban/ViewController.m
+++ Douban/Douban/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    NSLog(@"dfsdfsdfdsfdsf");
+    NSLog(@"dfsdfsdfdsfdsf的份儿");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
