6ac0605d83c6cd1974a1519420ff3ec6d5cc3df0
liulu <1628351982@qq.com>
2016-05-03 20:15:24 +0800

    控制程序

--- Douban/Douban/ViewController.m
+++ Douban/Douban/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    NSLog(@"dfsdfsdfdsfdsf的份儿56456546liulu123wuwengchang");
+    NSLog(@"dfsdfsdfdsfdsf的份儿56456546chang");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
