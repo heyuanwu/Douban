c2bbbb6fbcb053acec8306be53d1d912875e341b
liman <837844229@qq.com>
2016-05-03 20:02:35 +0800

    什么

--- Douban/Douban/ViewController.m
+++ Douban/Douban/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    NSLog(@"dfsdfsdfdsfdsf的份儿56456546");
+    NSLog(@"dfsdfsdfdsfdsf的份儿56456542464566");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
