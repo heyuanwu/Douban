144611744aac2af2a6111358aef31c71b7bdabe9
lkjy <1443799603@qq.com>
2016-05-06 21:05:36 +0800

    登录界面和sidebar界面

--- Douban/Douban/UserInfo.h
+++ Douban/Douban/UserInfo.h
@@ -8,6 +8,24 @@
 
 #import <Foundation/Foundation.h>
 
-@interface UserInfo : NSObject
+@interface UserInfo : NSObject<NSCoding>//NSCoding编码
+
+@property(nonatomic,copy)NSString* isNotLogin;
+
+@property(nonatomic,copy)NSString* cookies;
+
+@property(nonatomic,copy)NSString* userID;//用户身份
+@property(nonatomic,copy)NSString* name;//
+
+//歌曲里面的禁止、喜欢、播放歌曲列表的属性
+@property(nonatomic,copy)NSString* banned;//禁止
+
+@property(nonatomic,copy)NSString* liked;
+
+@property(nonatomic,copy)NSString* played;
+
+-(instancetype)initWithDictionary:(NSDictionary*)dictionary;
+-(void)archiverUserInfo;//存储用户信息
+
 
 @end
--- Douban/Douban/UserInforViewController.m
+++ Douban/Douban/UserInforViewController.m
@@ -36,17 +36,92 @@
     
 }
 
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+
+    [self setUserInfo];
+}
+
+-(void)viewDidLayoutSubviews
+{
+    [super viewDidLayoutSubviews];
+
+    _loginImage.layer.cornerRadius=_loginImage.frame.size.width/2.0;
+    if (!_loginImage.clipsToBounds)
+    {
+        _loginImage.clipsToBounds=YES;
+    }
+}
 
 
 -(void)loginImageTapped
 {
+    LoginViewContorllerViewController* loginVC=[mainStoryboard instantiateViewControllerWithIdentifier:@"loginVC"];
+
+    loginVC.delgate=(id)self;
+   // [[CDSideBarController sharedInstance]dismissMenu];
+    [self presentViewController:loginVC animated:YES completion:nil];
+}
+//注
+-(void)alertView:(UIAlertView*)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    switch (buttonIndex)
+    {
+        case 0:
+            break;
+        case 1:
+            [networkmanager logout];
+            break;
+        default:
+            break;
+    }
+    
 
 }
+
+
+
+
+//设置用户信息是从userInfo里获取
 -(void)setUserInfo
 {
+    //如果已经登录过了，直接显示信息
+    if (appDelegate.userInfo.cookies)
+    {
+        [_loginImage setImageWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://img3.douban.com/icon/ul%@-1.jpg",appDelegate.userInfo.userID]]];
+        
+        _loginImage.userInteractionEnabled=NO;//不允许交互
+        _usernameLabel.text=appDelegate.userInfo.name;
+        
+        _playedLabel.text=appDelegate.userInfo.played;
+        
+        _likedLabel.text=appDelegate.userInfo.liked;
+        
+        _bannedLabel.text=appDelegate.userInfo.banned;
+        
+        _logoutButton.hidden=NO;
+    
+    }
+    //如果没有登录，显示的信息
+    else
+    {
+        [_loginImage setImage:[UIImage imageNamed:@"login"]];
+    
+        _loginImage.userInteractionEnabled=YES;
+        _usernameLabel.text=@"";
+        _playedLabel.text=@"0";
+        _likedLabel.text=@"0";
+        _bannedLabel.text=@"0";
+        _logoutButton.hidden=YES;
+    
+    }
+    
+    
 }
 -(void)logoutSuccess
 {
+    [self setUserInfo];
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -63,6 +138,11 @@
 }
 */
 
-- (IBAction)logoutButtonTapped:(UIButton *)sender {
+//点击登出按钮响应的事件
+- (IBAction)logoutButtonTapped:(UIButton *)sender
+{
+    UIAlertView* alertView=[[UIAlertView alloc] initWithTitle:@"登出" message:@"您确定要登出吗" delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"确定", nil];
+    [alertView show];
+    
 }
 @end
